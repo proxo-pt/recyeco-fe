be5e5a90b3113b62b7ee11c2f860f3b08063a094
zkyzzz <muhadlim17@gmail.com>
2024-03-06 16:11:01 +0800

    feat: Setup for Recyeco Mart project

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import { NextPage } from 'next';
+import SignInPage from '@/features/Auth/Sign-in';
+
+const SignIn: FC<NextPage> = () => {
+  return <SignInPage />;
+};
+export default SignIn;
---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import { NextPage } from 'next';
+import SignUpPage from '@/features/Auth/Sign-up';
+
+const SignUp: FC<NextPage> = () => {
+  return <SignUpPage />;
+};
+export default SignUp;
