f8e6ce0af62c487e870c169951ce3ea78cdbf1f4
Farhan <anak.upload90@gmail.com>
2024-03-07 22:52:33 +0800

    refactor: Customize Input Component Style

--- src/components/ui/input-password.tsx
+++ src/components/ui/input-password.tsx
@@ -15,30 +15,38 @@ const InputPassword = React.forwardRef<HTMLInputElement, InputProps>(
     const [isFocused, setIsFocused] = React.useState(false);
     const [typePass, setTypePass] = React.useState(type);
     return (
-      <div
+      <label
         className={cn(
-          'flex items-center gap-2 h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-          isFocused && 'border border-blue-400'
+          'flex items-center gap-4 w-full h-14 rounded-xl border border-black bg-transparent placeholder-black px-6 py-3 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          isFocused && 'outline outline-1'
         )}
       >
         {suffix}
         <input
           type={typePass}
-          className={cn('w-full outline-none', className)}
+          className={cn(
+            'w-full h-full outline-none placeholder:text-black bg-transparent text-base font-light px-2',
+            className
+          )}
           ref={ref}
           onFocus={() => setIsFocused(true)}
           onBlur={() => setIsFocused(false)}
           {...props}
         />
         {typePass === 'password' ? (
-          <Eye className={colorIcon} onClick={() => setTypePass('text')} />
+          <Eye
+            className={colorIcon}
+            strokeWidth={'1'}
+            onClick={() => setTypePass('text')}
+          />
         ) : (
           <EyeOff
             className={colorIcon}
+            strokeWidth={'1'}
             onClick={() => setTypePass('password')}
           />
         )}
-      </div>
+      </label>
     );
   }
 );
