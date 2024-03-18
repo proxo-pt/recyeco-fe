5a522883df2bffaacbfdb8ecd1f409b4ddc80d3b
zkyzzz <muhadlim17@gmail.com>
2024-03-18 23:25:27 +0800

    chore: Move sign-in/up to Auth

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { NextPage } from 'next';
-import SignInPage from '@/features/Auth/Sign-in';
+import SignInPage from '@/features/Auth/sign-in';
 
 const SignIn: FC<NextPage> = () => {
   return <SignInPage />;
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { NextPage } from 'next';
-import SignUpPage from '@/features/Auth/Sign-up';
+import SignUpPage from '@/features/Auth/sign-up';
 
 const SignUp: FC<NextPage> = () => {
   return <SignUpPage />;
