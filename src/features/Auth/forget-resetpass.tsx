b44c244f6a25a3364b27f1b9b1395ab82a659638
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-03-24 12:22:35 +0800

    feat: Slicing Forget, Reset, and New Password Page

---
+++ src/features/Auth/forget-resetpass.tsx
@@ -0,0 +1,46 @@
+import { Button } from '@/components/ui/button';
+import { getAssetUrl } from '@/lib/utils';
+import Image from 'next/image';
+import { FC } from 'react';
+
+const ForgetResetPass: FC = () => {
+  return (
+    <div className="flex justify-between h-screen">
+      <div className="hidden lg:flex justify-center items-center w-full bg-gradient-to-b from-[#61C580] to-[#C7F7AE]">
+        <Image
+          priority
+          src={getAssetUrl('/auth/forgetnewpass.svg')}
+          height={0}
+          width={0}
+          className="xl:w-9/12 xl:h-9/12 md:w-11/12 md:h-11/12"
+          alt="forget-pass background"
+        />
+      </div>
+      <div className="flex flex-col gap-7 justify-center items-center w-full bg-[#DCDCDC]">
+        <div className="w-8/12 text-right">
+          <h1 className="text-3xl font-semibold mb-3">Periksa email Anda</h1>
+          <p className=" font-medium">
+            Kami telah mengirimkan email berisi informasi pengaturan ulang kata
+            sandi ke proxo@gmail.com
+          </p>
+          <p className="text-right mt-8">
+            Tidak menerima emailnya? periksa folder spam atau
+          </p>
+        </div>
+        <div className="flex flex-col gap-5 w-8/12">
+          <Button size={'login'} className="bg-[#4F4F4F] hover:bg-neutral-500">
+            Kirim Ulang
+          </Button>
+          <Button
+            size={'login'}
+            className="bg-[#DCDCDC] text-black border-black border-2 hover:bg-[#4F4F4F] hover:border-0 hover:text-white"
+          >
+            Kembali
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ForgetResetPass;
