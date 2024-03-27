4ad6f1d60ae3a50c94840e64914afe6a7e68e0ae
Farhan <anak.upload90@gmail.com>
2024-03-27 13:49:09 +0800

    feat: create card item component

---
+++ src/constants/event.ts
@@ -0,0 +1,94 @@
+import { EventType } from '@/types';
+
+export const Event: EventType[] = [
+  {
+    id: 1,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    point: 2000,
+    date: '2024-05-11',
+    slot: 12,
+    location: 'Jln. suharto. No 34, palu selatan'
+  },
+  {
+    id: 2,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    point: 2000,
+    date: '2024-05-11',
+    slot: 11,
+    location: 'Jln. suharto. No 34, palu selatan'
+  },
+  {
+    id: 3,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    point: 2000,
+    date: '2024-05-11',
+    slot: 12,
+    location: 'Jln. suharto. No 34, palu selatan'
+  },
+  {
+    id: 4,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    point: 2000,
+    date: '2024-05-11',
+    slot: 12,
+    location: 'Jln. suharto. No 34, palu selatan'
+  },
+  {
+    id: 5,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    point: 2000,
+    date: '2024-05-11',
+    slot: 12,
+    location: 'Jln. suharto. No 34, palu selatan'
+  },
+  {
+    id: 6,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    point: 2000,
+    date: '2024-05-11',
+    slot: 12,
+    location: 'Jln. suharto. No 34, palu selatan'
+  },
+  {
+    id: 7,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    point: 2000,
+    date: '2024-05-11',
+    slot: 12,
+    location: 'Jln. suharto. No 34, palu selatan'
+  },
+  {
+    id: 8,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    point: 2000,
+    date: '2024-05-11',
+    slot: 12,
+    location: 'Jln. suharto. No 34, palu selatan'
+  },
+  {
+    id: 9,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    point: 2000,
+    date: '2024-05-11',
+    slot: 12,
+    location: 'Jln. suharto. No 34, palu selatan'
+  },
+  {
+    id: 10,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    point: 2000,
+    date: '2024-05-11',
+    slot: 12,
+    location: 'Jln. suharto. No 34, palu selatan'
+  }
+];
---
+++ src/constants/items.ts
@@ -0,0 +1,84 @@
+import { ItemsType } from '@/types';
+
+export const Items: ItemsType[] = [
+  {
+    id: 1,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    price: 12000,
+    weight: 12,
+    location: 'Jln. suharto. No 34, palu selatan'
+  },
+  {
+    id: 2,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    price: 12000,
+    weight: 12,
+    location: 'Jln. suharto. No 34, palu selatan'
+  },
+  {
+    id: 3,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    price: 12000,
+    weight: 12,
+    location: 'Jln. suharto. No 34, palu selatan'
+  },
+  {
+    id: 4,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    price: 12000,
+    weight: 12,
+    location: 'Jln. suharto. No 34, palu selatan'
+  },
+  {
+    id: 5,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    price: 12000,
+    weight: 12,
+    location: 'Jln. suharto. No 34, palu selatan'
+  },
+  {
+    id: 6,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    price: 12000,
+    weight: 12,
+    location: 'Jln. suharto. No 34, palu selatan'
+  },
+  {
+    id: 7,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    price: 12000,
+    weight: 12,
+    location: 'Jln. suharto. No 34, palu selatan'
+  },
+  {
+    id: 8,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    price: 12000,
+    weight: 12,
+    location: 'Jln. suharto. No 34, palu selatan'
+  },
+  {
+    id: 9,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    price: 12000,
+    weight: 12,
+    location: 'Jln. suharto. No 34, palu selatan'
+  },
+  {
+    id: 10,
+    img: 'dummy-sampah.jpg',
+    title: 'Tittle maximal 2 line LOREM KFKDMMEK CE JNTJNRVNRJNVJNNRNN',
+    price: 12000,
+    weight: 12,
+    location: 'Jln. suharto. No 34, palu selatan'
+  }
+];
