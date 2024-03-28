33b2d7d41fdfda5fc32325ae84509892a50b2e13
Farhan <anak.upload90@gmail.com>
2024-03-29 04:33:38 +0800

    feat: Create Products Page

---
+++ src/app/product/page.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import { NextPage } from 'next';
+import ProductsPage from '@/features/Products';
+
+const Products: FC<NextPage> = () => {
+  return <ProductsPage />;
+};
+export default Products;
---
+++ src/features/Products/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Products = () => {
+  return <div>detail products</div>;
+};
+
+export default Products;
