e27e21e70891d15105881b106bd02f6c3d627727
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-04-02 22:00:31 +0800

    move footer into global component

--- src/features/Main/index.tsx
+++ src/features/Main/index.tsx
@@ -1,4 +1,4 @@
-import Footer from './components/Footer';
+import Footer from '../../components/layout/Footer';
 import Dashboard from './sections/Dashboard';
 import Market from './sections/Market';
 
