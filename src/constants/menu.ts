0576ab49ca25f9ba7c6abdee3f23cdfcd6361196
Farhan <anak.upload90@gmail.com>
2024-04-04 21:32:57 +0800

    chore: clean up duplicate constant

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -1,4 +1,4 @@
-import { CategoryItem, IklanItem, MainMenuType, CategoryColor } from '@/types';
+import { CategoryItem, IklanItem, MainMenuType } from '@/types';
 
 export const MainMenu: MainMenuType[] = [
   {
@@ -15,76 +15,7 @@ export const MainMenu: MainMenuType[] = [
   }
 ];
 
-export const CategoryItems: CategoryItem[] = [
-  {
-    id: 1,
-    backgroundColor: 'bg-[#3B97D326]',
-    borderColor: 'hover:border-[#3B97D3]',
-    label: 'Plastik'
-  },
-  {
-    id: 2,
-    backgroundColor: 'bg-[#35CC3326]',
-    borderColor: 'hover:border-[#35CC33]',
-    label: 'Kaca'
-  },
-  {
-    id: 3,
-    backgroundColor: 'bg-[#8A8B8E26]',
-    borderColor: 'hover:border-[#8A8B8E]',
-    label: 'Logam'
-  },
-  {
-    id: 4,
-    backgroundColor: 'bg-[#C43B3B26]',
-    borderColor: 'hover:border-[#C43B3B]',
-    label: 'Kertas'
-  },
-  {
-    id: 5,
-    backgroundColor: 'bg-[#A0CC0026]',
-    borderColor: 'hover:border-[#A0CC00]',
-    label: 'Organik'
-  },
-  {
-    id: 6,
-    backgroundColor: 'bg-[#FFB65526]',
-    borderColor: 'hover:border-[#FFB655]',
-    label: 'Tekstil'
-  },
-  {
-    id: 7,
-    backgroundColor: 'bg-[#61788126]',
-    borderColor: 'hover:border-[#617881]',
-    label: 'Elektronik'
-  },
-  {
-    id: 8,
-    backgroundColor: 'bg-[#C6A51A26]',
-    borderColor: 'hover:border-[#C6A51A]',
-    label: 'Baterai'
-  }
-];
-
-export const IklanItems: IklanItem[] = [
-  {
-    id: 1,
-    backgroundImage: 'carouselJoin.svg',
-    title: 'Iklan Join'
-  },
-  {
-    id: 2,
-    backgroundImage: 'carouselToko.svg',
-    title: 'Iklan Toko'
-  },
-  {
-    id: 3,
-    backgroundImage: 'carouselDana.svg',
-    title: 'Iklan Dana'
-  }
-];
-
-export const CategoryColors: CategoryColor = {
+export const CategoryItems: CategoryItem = {
   Plastik: {
     backgroundColor: 'bg-[#3B97D326]',
     textColor: 'text-[#3B97D3]',
@@ -132,11 +63,23 @@ export const CategoryColors: CategoryColor = {
     textColor: 'text-[#C6A51A]',
     borderColor: 'border-[#C6A51A]',
     label: 'Baterai'
-  },
-  UNKNOWN: {
-    backgroundColor: '',
-    textColor: 'text-gray-500',
-    borderColor: 'border-gray-500',
-    label: 'Unknown'
   }
 };
+
+export const IklanItems: IklanItem[] = [
+  {
+    id: 1,
+    backgroundImage: 'carouselJoin.svg',
+    title: 'Iklan Join'
+  },
+  {
+    id: 2,
+    backgroundImage: 'carouselToko.svg',
+    title: 'Iklan Toko'
+  },
+  {
+    id: 3,
+    backgroundImage: 'carouselDana.svg',
+    title: 'Iklan Dana'
+  }
+];
