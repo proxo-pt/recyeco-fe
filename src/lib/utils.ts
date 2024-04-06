0dde32e56ba6228dfbb3f864e7621dbfe13d9ad9
adzaky <muhadlim17@gmail.com>
2024-04-06 15:09:16 +0800

    fix: Remove unused useState and client rendering

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,4 @@
-'use client';
 import { type ClassValue, clsx } from 'clsx';
-import { useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
