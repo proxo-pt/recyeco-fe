48247584e1ed50defd6943956bb55da4c62e7839
adzaky <muhadlim17@gmail.com>
2024-04-11 16:59:31 +0800

    chore: Update manage products and get shop data endpoint

--- src/service/product.ts
+++ src/service/product.ts
@@ -2,8 +2,8 @@ import { ProductPostResType, ProductResType } from '@/domains/product';
 import { fetcher } from './instance';
 
 export const ProductDataService = async (): Promise<any> => {
-  const res = await fetcher.get('postingan');
-  return res.data.postingan;
+  const res = await fetcher.get('manajemen');
+  return res.data.produk;
 };
 
 export const ProductPostService = async (data: FormData): Promise<any> => {
