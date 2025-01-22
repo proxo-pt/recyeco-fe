2a54205b282e9a5ccafba8dfab6f99bdb0ac88d5
adzaky <muhadlim17@gmail.com>
2025-01-23 04:15:28 +0800

    chore(home): add hover upscale ItemCard

--- src/features/home/components/ItemCard.tsx
+++ src/features/home/components/ItemCard.tsx
@@ -16,12 +16,19 @@ interface ItemCardProps {
 
 export default function ItemCard({ title, img, price, category, shopName, className }: ItemCardProps) {
   return (
-    <Card className={cn(`flex w-auto cursor-pointer flex-col rounded-xl drop-shadow-md`, className)}>
-      <div className="relative h-32 w-full md:h-52">
+    <Card
+      className={cn(`group flex w-auto cursor-pointer flex-col overflow-hidden rounded-xl drop-shadow-md`, className)}
+    >
+      <div className="relative h-32 w-full overflow-hidden md:h-52">
         <div className="absolute right-0 top-0 z-50 rounded-bl-[2rem] rounded-tr-xl bg-[#222D33] p-4 pr-3 pt-3 text-recyeco-primary">
           <ShoppingCart />
         </div>
-        <Image src={img ?? ""} alt={title} fill className="rounded-t-xl object-cover" />
+        <Image
+          src={img ?? ""}
+          alt={title}
+          fill
+          className="rounded-t-xl object-cover transition-transform group-hover:scale-105"
+        />
       </div>
       <CardHeader className="p-4 pb-1">
         <div className="flex items-center justify-between gap-2">
