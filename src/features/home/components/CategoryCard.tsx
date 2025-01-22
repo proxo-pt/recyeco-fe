ebf0e3c93c8227887bda12430d45a648e569cab9
adzaky <muhadlim17@gmail.com>
2025-01-23 04:10:46 +0800

    chore(home): update CategoryCard sizing style

--- src/features/home/components/CategoryCard.tsx
+++ src/features/home/components/CategoryCard.tsx
@@ -13,7 +13,13 @@ export default function CategoryCard({ src, alt, title, color }: CategoryCardPro
       className="group flex size-auto cursor-pointer flex-col items-center justify-center gap-1 rounded-lg border px-2 py-3 transition-colors hover:border-black"
       style={{ backgroundColor: color }}
     >
-      <Image src={src} alt={alt} width={32} height={32} className="transition-transform group-hover:scale-110" />
+      <Image
+        src={src}
+        alt={alt}
+        width={32}
+        height={32}
+        className="size-auto transition-transform group-hover:scale-110"
+      />
       <span className="text-xs text-gray-800">{title}</span>
     </div>
   );
