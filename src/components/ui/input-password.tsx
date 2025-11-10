f4c43cdd5ab75f6e80f79e48c57563d0ea1c7fb8
adzaky <muhadlim17@gmail.com>
2025-11-11 07:04:27 +0800

    fix: remove unnecessary placeholder color from input components

--- src/components/ui/input-password.tsx
+++ src/components/ui/input-password.tsx
@@ -17,7 +17,7 @@ const InputPassword = React.forwardRef<HTMLInputElement, InputProps>(
     return (
       <label
         className={cn(
-          'flex items-center gap-1 w-full h-14 rounded-xl border border-black bg-transparent placeholder-black px-4 py-3 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          'flex items-center gap-1 w-full h-14 rounded-xl border border-black bg-transparent px-4 py-3 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
           isFocused && 'outline outline-1'
         )}
       >
@@ -25,7 +25,7 @@ const InputPassword = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           type={typePass}
           className={cn(
-            'w-full h-full outline-none placeholder:text-black bg-transparent text-base font-light px-2',
+            'w-full h-full outline-none bg-transparent text-base font-light px-2',
             className
           )}
           ref={ref}
